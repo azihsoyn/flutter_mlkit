264d359106c4d1dfba082af54e7736b651e5713a
azihsoyn <azihsoyn@gmail.com>
2018-05-30 02:40:49 +0900

    miss commit

--- ios/Classes/MlkitPlugin.m
+++ ios/Classes/MlkitPlugin.m
@@ -1,5 +1,5 @@
 #import "MlkitPlugin.h"
-@import Firebase;
+#import "Firebase/Firebase.h"
 
 @implementation MlkitPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -10,26 +10,46 @@
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
+FIRVisionTextDetector *textDetector;
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    if (![FIRApp defaultApp]) {
+      [FIRApp configure];
+    }
+  }
+  return self;
+}
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  FIRVisionTextDetector *textDetector;
   FIRVision *vision = [FIRVision vision];
+  NSMutableArray *ret = [NSMutableArray array];
+
   textDetector = [vision textDetector];
+  NSString *path = call.arguments[@"filepath"];
+
+  UIImage* uiImage = [UIImage imageWithContentsOfFile:path];
   FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:uiImage];
   if ([@"detectFromPath" isEqualToString:call.method]) {
     [textDetector detectInImage:image
                      completion:^(NSArray<FIRVisionText *> *features,
                                   NSError *error) {
       if (error != nil) {
+        [ret addObject:error.localizedDescription];
+        result(ret);
         return;
       } else if (features != nil) {
         // Recognized text
         for (id <FIRVisionText> feature in features) {
           NSString *value = feature.text;
-          NSArray<NSValue *> *corners = feature.cornerPoints;
+          //NSArray<NSValue *> *corners = feature.cornerPoints;
+          [ret addObject:value];
         }
       }
+      result(ret);
+      return;
     }];
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else {
     result(FlutterMethodNotImplemented);
   }
