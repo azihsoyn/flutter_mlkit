f8fbdb029d1059d0fd3c8bdc4d75ee0414dd9d21
Naoya Yoshizawa <azihsoyn@gmail.com>
2018-06-04 23:36:42 +0900

    implement barcode scan for Android (#13)
    * implement barcode scan for Android
    
    * update docs and yaml

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1
+
+* fully support Scan barcodes on device.
+
 ## 0.2.0
 
 * support Scan barcodes (iOS only)
--- android/src/main/java/com/azihsoyn/flutter/mlkit/MlkitPlugin.java
+++ android/src/main/java/com/azihsoyn/flutter/mlkit/MlkitPlugin.java
@@ -10,12 +10,16 @@ import io.flutter.view.FlutterView;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import android.support.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
@@ -80,9 +84,184 @@ public class MlkitPlugin implements MethodCallHandler {
         Log.e("error", e.getMessage());
         return;
       }
+    } else if (call.method.equals("FirebaseVisionBarcodeDetector#detectFromPath")) {
+      String path = call.argument("filepath");
+      try {
+        File file = new File(path);
+        FirebaseVisionImage image = FirebaseVisionImage.fromFilePath(context, Uri.fromFile(file));
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+        detector.detectInImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                          @Override
+                          public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                            result.success(processBarcodeRecognitionResult(barcodes));
+                          }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                          @Override
+                          public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            e.printStackTrace();
+                          }
+                        });
+      } catch (IOException e) {
+        Log.e("error", e.getMessage());
+        return;
+      }
     } else {
       result.notImplemented();
     }
+  }
+
+  private ImmutableList<ImmutableMap<String, Object>> processBarcodeRecognitionResult(List<FirebaseVisionBarcode> barcodes) {
+    ImmutableList.Builder<ImmutableMap<String, Object>> dataBuilder =
+            ImmutableList.<ImmutableMap<String, Object>>builder();
+
+    for (FirebaseVisionBarcode barcode: barcodes) {
+      ImmutableMap.Builder<String, Object> barcodeBuilder = ImmutableMap.<String, Object>builder();
+
+      Rect bounds = barcode.getBoundingBox();
+      barcodeBuilder.put("rect_bottom", (double)bounds.bottom);
+      barcodeBuilder.put("rect_top", (double)bounds.top);
+      barcodeBuilder.put("rect_right", (double)bounds.right);
+      barcodeBuilder.put("rect_left", (double)bounds.left);
+
+      ImmutableList.Builder<ImmutableMap<String, Integer>> pointsBuilder =
+              ImmutableList.<ImmutableMap<String, Integer>>builder();
+      for (Point corner : barcode.getCornerPoints()) {
+        ImmutableMap.Builder<String, Integer> pointBuilder = ImmutableMap.<String, Integer>builder();
+        pointBuilder.put("x", corner.x);
+        pointBuilder.put("y", corner.y);
+        pointsBuilder.add(pointBuilder.build());
+      }
+      barcodeBuilder.put("points", pointsBuilder.build());
+      barcodeBuilder.put("raw_value", barcode.getRawValue());
+      barcodeBuilder.put("display_value", barcode.getDisplayValue());
+      barcodeBuilder.put("format", barcode.getFormat());
+
+      int valueType = barcode.getValueType();
+      barcodeBuilder.put("value_type", valueType);
+
+      ImmutableMap.Builder<String, Object> typeValueBuilder = ImmutableMap.<String, Object>builder();
+      switch (valueType) {
+        case FirebaseVisionBarcode.TYPE_EMAIL:
+          typeValueBuilder.put("type", barcode.getEmail().getType());
+          typeValueBuilder.put("address", barcode.getEmail().getAddress());
+          typeValueBuilder.put("body", barcode.getEmail().getBody());
+          typeValueBuilder.put("subject", barcode.getEmail().getSubject());
+          barcodeBuilder.put("email", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_PHONE:
+          typeValueBuilder.put("number", barcode.getPhone().getNumber());
+          typeValueBuilder.put("type", barcode.getPhone().getType());
+          barcodeBuilder.put("phone", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_SMS:
+          typeValueBuilder.put("message", barcode.getSms().getMessage());
+          typeValueBuilder.put("phone_number", barcode.getSms().getPhoneNumber());
+          barcodeBuilder.put("sms", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_URL:
+          typeValueBuilder.put("title", barcode.getUrl().getTitle());
+          typeValueBuilder.put("url", barcode.getUrl().getUrl());
+          barcodeBuilder.put("url", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_WIFI:
+          typeValueBuilder.put("ssid", barcode.getWifi().getSsid());
+          typeValueBuilder.put("password", barcode.getWifi().getPassword());
+          typeValueBuilder.put("encryption_type", barcode.getWifi().getEncryptionType());
+          barcodeBuilder.put("wifi", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_GEO:
+          typeValueBuilder.put("latitude", barcode.getGeoPoint().getLat());
+          typeValueBuilder.put("longitude", barcode.getGeoPoint().getLng());
+          barcodeBuilder.put("geo_point", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_CONTACT_INFO:
+          ImmutableList.Builder<ImmutableMap<String, Object>> addressesBuilder =
+                  ImmutableList.builder();
+          for (FirebaseVisionBarcode.Address address : barcode.getContactInfo().getAddresses()) {
+            ImmutableMap.Builder<String, Object> addressBuilder = ImmutableMap.builder();
+            addressBuilder.put("address_lines", address.getAddressLines());
+            addressBuilder.put("type", address.getType());
+            addressesBuilder.add(addressBuilder.build());
+          }
+          typeValueBuilder.put("addresses", addressesBuilder.build());
+
+          ImmutableList.Builder<ImmutableMap<String, Object>> emailsBuilder =
+                  ImmutableList.builder();
+          for (FirebaseVisionBarcode.Email email : barcode.getContactInfo().getEmails()) {
+            ImmutableMap.Builder<String, Object> emailBuilder = ImmutableMap.builder();
+            emailBuilder.put("address", email.getAddress());
+            emailBuilder.put("type", email.getType());
+            emailBuilder.put("body", email.getBody());
+            emailBuilder.put("subject", email.getSubject());
+            emailsBuilder.add(emailBuilder.build());
+          }
+          typeValueBuilder.put("emails", emailsBuilder.build());
+
+          ImmutableMap.Builder<String, Object> nameBuilder = ImmutableMap.builder();
+          nameBuilder.put("formatted_name",  barcode.getContactInfo().getName().getFormattedName());
+          nameBuilder.put("first", barcode.getContactInfo().getName().getFirst());
+          nameBuilder.put("last", barcode.getContactInfo().getName().getLast());
+          nameBuilder.put("middle", barcode.getContactInfo().getName().getMiddle());
+          nameBuilder.put("prefix", barcode.getContactInfo().getName().getPrefix());
+          nameBuilder.put("pronounciation", barcode.getContactInfo().getName().getPronunciation());
+          nameBuilder.put("suffix", barcode.getContactInfo().getName().getSuffix());
+          typeValueBuilder.put("name", nameBuilder.build());
+
+
+          ImmutableList.Builder<ImmutableMap<String, Object>> phonesBuilder =
+                  ImmutableList.builder();
+          for (FirebaseVisionBarcode.Phone phone : barcode.getContactInfo().getPhones()) {
+            ImmutableMap.Builder<String, Object> phoneBuilder = ImmutableMap.builder();
+            phoneBuilder.put("number", phone.getNumber());
+            phoneBuilder.put("type", phone.getType());
+            phonesBuilder.add(phoneBuilder.build());
+          }
+          typeValueBuilder.put("phones", phonesBuilder.build());
+
+          typeValueBuilder.put("urls", barcode.getContactInfo().getUrls());
+          typeValueBuilder.put("job_title", barcode.getContactInfo().getTitle());
+          typeValueBuilder.put("organization", barcode.getContactInfo().getOrganization());
+
+          barcodeBuilder.put("contact_info", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_CALENDAR_EVENT:
+          typeValueBuilder.put("event_description", barcode.getCalendarEvent().getDescription());
+          typeValueBuilder.put("location", barcode.getCalendarEvent().getLocation());
+          typeValueBuilder.put("organizer", barcode.getCalendarEvent().getOrganizer());
+          typeValueBuilder.put("status", barcode.getCalendarEvent().getStatus());
+          typeValueBuilder.put("summary", barcode.getCalendarEvent().getSummary());
+          typeValueBuilder.put("start", barcode.getCalendarEvent().getStart());
+          typeValueBuilder.put("end", barcode.getCalendarEvent().getEnd());
+          barcodeBuilder.put("calendar_event", typeValueBuilder.build());
+          break;
+        case FirebaseVisionBarcode.TYPE_DRIVER_LICENSE:
+          typeValueBuilder.put("first_name", barcode.getDriverLicense().getFirstName());
+          typeValueBuilder.put("middle_name", barcode.getDriverLicense().getMiddleName());
+          typeValueBuilder.put("last_name", barcode.getDriverLicense().getLastName());
+          typeValueBuilder.put("gender", barcode.getDriverLicense().getGender());
+          typeValueBuilder.put("address_city", barcode.getDriverLicense().getAddressCity());
+          typeValueBuilder.put("address_state", barcode.getDriverLicense().getAddressState());
+          typeValueBuilder.put("address_zip", barcode.getDriverLicense().getAddressZip());
+          typeValueBuilder.put("birth_date", barcode.getDriverLicense().getBirthDate());
+          typeValueBuilder.put("document_type", barcode.getDriverLicense().getDocumentType());
+          typeValueBuilder.put("license_number", barcode.getDriverLicense().getLicenseNumber());
+          typeValueBuilder.put("expiry_date", barcode.getDriverLicense().getExpiryDate());
+          typeValueBuilder.put("issuing_date", barcode.getDriverLicense().getIssueDate());
+          typeValueBuilder.put("issuing_country", barcode.getDriverLicense().getIssuingCountry());
+          barcodeBuilder.put("calendar_event", typeValueBuilder.build());
+          break;
+      }
+
+      dataBuilder.add(barcodeBuilder.build());
+    }
+
+    return dataBuilder.build();
   }
 
   private ImmutableList<ImmutableMap<String, Object>> processTextRecognitionResult(FirebaseVisionText texts) {
@@ -103,10 +282,10 @@ public class MlkitPlugin implements MethodCallHandler {
       ImmutableList.Builder<ImmutableMap<String, Integer>> blockPointsBuilder =
               ImmutableList.<ImmutableMap<String, Integer>>builder();
       for (int p = 0; p < blocks.get(i).getCornerPoints().length; p++) {
-          ImmutableMap.Builder<String, Integer> pointBuilder = ImmutableMap.<String, Integer>builder();
-          pointBuilder.put("x", blocks.get(i).getCornerPoints()[p].x);
-          pointBuilder.put("y", blocks.get(i).getCornerPoints()[p].y);
-          blockPointsBuilder.add(pointBuilder.build());
+        ImmutableMap.Builder<String, Integer> pointBuilder = ImmutableMap.<String, Integer>builder();
+        pointBuilder.put("x", blocks.get(i).getCornerPoints()[p].x);
+        pointBuilder.put("y", blocks.get(i).getCornerPoints()[p].y);
+        blockPointsBuilder.add(pointBuilder.build());
       }
       blockBuilder.put("points", blockPointsBuilder.build());
 
